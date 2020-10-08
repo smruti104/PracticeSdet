206f82cc0890094095f38cf28f23e338f6668d48
smruti104 <nandysmruti@gmail.com>
2020-10-08 14:39:56 +0530

    class 1 is added

---
+++ src/com/practice/Demo2.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String s=new String("Qspider");
+		System.out.println(s);
+		s.concat("_Coaching");
+		System.out.println(s);
+
+	}
+
+}
---
+++ src/com/practice/Test1.java
@@ -0,0 +1,7 @@
+package com.practice;
+
+public class Test1 {
+public static void main(String[] args) {
+	System.err.println("Welcome to my new house");
+}
+}
