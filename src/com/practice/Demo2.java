b0e94e242a2ed6c832822365d2399e9027ce14b3
smruti104 <nandysmruti@gmail.com>
2020-10-08 14:54:19 +0530

    class 5 added and 1 modified

--- src/com/practice/Demo2.java
+++ src/com/practice/Demo2.java
@@ -3,7 +3,7 @@ package com.practice;
 public class Demo2 {
 
 	public static void main(String[] args) {
-		String s=new String("Qspider");
+		String s=new String("jspider");
 		System.out.println(s);
 		s.concat("_Coaching");
 		System.out.println(s);
---
+++ src/com/practice/Test5.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
