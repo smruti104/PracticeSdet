316f2324a73d8c5b66e098b91af1ae82afb7db90
smruti104 <nandysmruti@gmail.com>
2020-10-08 14:11:16 +0530

    class 1 is added

---
+++ src/com/practice/Demo.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String s1=new String("welcome");
+		String s2="GoodMorning";
+		
+		System.out.println(s1==s2);
+		
+
+	}
+
+}
